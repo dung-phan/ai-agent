d434b54ef781e25595f829b5a477de895356581e
dung-phan <dungphan@pm.me>
2025-06-14 08:23:22 +0200

    Adds verbose flag to print more info

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
 
     client = genai.Client(api_key=api_key)
     args = sys.argv[1:]
+    is_verbose =  "--verbose" in args
 
     if not args:
         print("No prompt provided. Please provide a prompt.")
@@ -19,11 +20,19 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)])
     ]
+
+    if is_verbose:
+        print('User prompt:', prompt)
+
+    generate_content(client, messages, is_verbose)
+
+def generate_content(client, messages, is_verbose):
     res = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
     token_usage = res.usage_metadata
-    print('prompt:', res.text)
-    print("Prompt tokens:", token_usage.prompt_token_count)
-    print("Response tokens:", token_usage.candidates_token_count)
+
+    if is_verbose:
+        print("Prompt tokens:", token_usage.prompt_token_count)
+        print("Response tokens:", token_usage.candidates_token_count)
 
 if __name__ == "__main__":
     main()
