0bc544d9f23b720af1e4ec627b4887bbbf21b090
dung-phan <dungphan@pm.me>
2025-06-14 12:32:59 +0200

    Gets the contents of a dir

---
+++ functions/get_files_info.py
@@ -0,0 +1,35 @@
+import os
+
+def get_files_info(working_dir, directory=None):
+    try:
+        current_dir = '' if directory is None else directory
+        working_dir_path = os.path.abspath(working_dir)
+        merged_path = os.path.abspath(os.path.join(working_dir, current_dir))
+        is_dir = os.path.isdir(merged_path)
+
+        if not (merged_path.startswith(working_dir_path + os.sep) or merged_path == working_dir_path):
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        elif not is_dir:
+            return f'Error: "{directory}" is not a directory'
+
+        return generate_dir_content(merged_path)
+
+    except Exception as e:
+        return f"Error: {e}"
+
+def generate_dir_content(path):
+    try:
+        files = os.listdir(path)
+
+        def get_file_info(file):
+            file_path = os.path.join(path, file)
+            return f"- {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}"
+
+        return "\n".join(list((map(get_file_info,files))))
+
+    except Exception as e:
+        return f"Error: {e}"
+
+
+
+
