bc9661da1c301b15c63fde15f74163cff3d52e72
dung-phan <dungphan@pm.me>
2025-06-15 21:16:40 +0200

    Allows iteration of feedback loops

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 from google.genai import types
 import sys
 
+from config import MAX_ITERATION
 from genai_call_function import available_functions, call_function
 from genai_prompts import system_prompt
 
@@ -28,7 +29,22 @@ def main():
     if is_verbose:
         print('User prompt:', prompt)
 
-    generate_content(client, messages, is_verbose)
+    for _ in range(MAX_ITERATION):
+        response = generate_content(client, messages, is_verbose)
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+        if response.function_calls:
+            for f in response.function_calls:
+                result = call_function(f, is_verbose)
+                messages.append(result)
+                if not result.parts[0] or not result.parts[0].function_response:
+                    raise Exception("Empty function call result")
+                if is_verbose:
+                    print(f"-> {result.parts[0].function_response.response['result']}")
+        elif response.text:
+            print("Result", response.text)
+            break
+
 
 def generate_content(client, messages, is_verbose):
     res = client.models.generate_content(
@@ -42,19 +58,9 @@ def generate_content(client, messages, is_verbose):
     if is_verbose:
         print("Prompt tokens:", token_usage.prompt_token_count)
         print("Response tokens:", token_usage.candidates_token_count)
-
-
-    if res.function_calls:
-        for f in res.function_calls:
-            result = call_function(f, is_verbose)
-            if not result.parts[0] or not result.parts[0].function_response:
-                raise Exception("Empty function call result")
-            if is_verbose:
-                print(f"-> {result.parts[0].function_response.response['result']}")
-
     else:
         print("Response:", res.text)
 
-
+    return res
 if __name__ == "__main__":
     main()
