aaca19c593460d32885786ebc06eefc67cdeec8d
dung-phan <dungphan@pm.me>
2025-06-15 15:45:21 +0200

    Declares functions for LLM to call

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from google import genai
 from google.genai import types
 import sys
 
+from genai_call_function import available_functions
+from genai_prompts import system_prompt
+
+
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -27,12 +31,25 @@ def main():
     generate_content(client, messages, is_verbose)
 
 def generate_content(client, messages, is_verbose):
-    res = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    res = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions],system_instruction=system_prompt)
+    )
+
     token_usage = res.usage_metadata
 
     if is_verbose:
         print("Prompt tokens:", token_usage.prompt_token_count)
         print("Response tokens:", token_usage.candidates_token_count)
+
+
+    if res.function_calls:
+        for f in res.function_calls:
+            print(f"Calling function: {f.name}({f.args})")
+    else:
+        print("Response:", res.text)
+
 
 if __name__ == "__main__":
     main()
